4431a0fba5ab4b0bce535dd7aae0cc910ad77836
satoru <ksk1889satoru18@i.softbank.jp>
2024-06-07 10:19:05 +0100

    set up db

--- app.js
+++ app.js
@@ -2,34 +2,45 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const port = 3000;
+const config = require("./config/mysql.config.js");
 
-const con = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "3106kmkaio",
-  database: "food_protein"
+//viewエンジン設定
+app.set("view engine", "ejs");
+app.use("/", require("./routes/index.js"))
+
+//static rooting
+app.use(express.static('public'));
+
+//db取得
+app.use("/test", async(req, res, next) =>{
+  const {promisify} = require("util");
+  const con = mysql.createConnection({
+    host: config.HOST,
+    port: config.PORT,
+    user: config.USERNAME,
+    password: config.PASSWORD,
+    database: config.DATABASE
+  });
+
+  const client = {
+    connect: promisify(con.connect).bind(con),
+    query: promisify(con.query).bind(con),
+    end: promisify(con.end).bind(con),
+  };
+  var data;
+
+  try {
+    await client.connect();
+    data = await client.query('SELECT * FROM foodlist');
+    console.log(data);
+  } catch (err) {
+    next(err);
+  } finally {
+    await client.end();
+  }
+  res.end("ok");
 });
-
-con.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('success');
-  });
-
-  app.get('/', (req, res) => {
-    con.query(
-      'SELECT * FROM foodlist',
-      (error, results) => {
-        console.log(results);
-        res.send('エラーが発生しています');
-      }
-    );
-  });
   
 app.listen(port, () => {
     console.log(`このサーバーは ${port}に接続されています`);
   });
-
