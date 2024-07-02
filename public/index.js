3862b0ee5415e00e1bcf8adec99104bc6214c455
SomaTomita <ldh.eeexile@icloud.com>
2024-07-02 23:23:54 +0900

    修正 タンパク質の総量
    before:
    const foodData = data.find(item => item.id === parseInt(food.id));
    after:
    const foodData = data.find(item => item.name == food.id);

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
+
 let data;
 let totalProtein;
 
 document.addEventListener('DOMContentLoaded', () => {
-//バックエンドへデータ要求
+  //バックエンドへデータ要求
   axios.get('/api/foodlist')
     .then(response => {
       data = response.data;
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         option.value = food.name;
         option.textContent = `${food.name} (${food.protein_per_100g}g/100g)`;
         foodSelect.appendChild(option);
-        });
+      });
     })
     .catch(error => console.error('Error fetching food list:', error));
 });
@@ -21,7 +22,7 @@ const addbtn = document.getElementById('add-button');
 const foodList = document.getElementById('ingredient-list');
 let selectedFoods = [];
 
- addbtn.addEventListener('click', function() {
+addbtn.addEventListener('click', function () {
   const ingredientInput = document.getElementById('ingredient-input');
   const amountInput = document.getElementById('amount-input');
   const foodId = ingredientInput.value;
@@ -29,22 +30,22 @@ let selectedFoods = [];
   const amountTxt = parseInt(amountInput.value);
 
   if (foodId && amountTxt) {
-    selectedFoods.push({id: foodId, amount: amountTxt});
+    selectedFoods.push({ id: foodId, amount: amountTxt });
     const list = document.createElement('li');
     list.textContent = `${foodTxt} ${amountTxt}g`;
     foodList.appendChild(list);
     console.log(selectedFoods);
-    
- // automatic calculate
- totalProtein = 0;
- selectedFoods.forEach(food => {
-   const foodData = data.find(item => item.id === parseInt(food.id));
-   if (foodData) {
-     totalProtein += (food.amount / 100) * foodData.protein_per_100g;
-   }
- });
- let totalProteinDisplay = document.getElementById('total-protein');
- totalProteinDisplay.textContent = `タンパク質の総量: ${totalProtein.toFixed(2)}g`;
+
+    // automatic calculate
+    totalProtein = 0;
+    selectedFoods.forEach(food => {
+      const foodData = data.find(item => item.name == food.id);
+      if (foodData) {
+        totalProtein += (food.amount / 100) * foodData.protein_per_100g;
+      }
+    });
+    let totalProteinDisplay = document.getElementById('total-protein');
+    totalProteinDisplay.textContent = `タンパク質の総量: ${totalProtein.toFixed(2)}g`;
   } else {
     alert('食材名と量を入力してください');
   }
